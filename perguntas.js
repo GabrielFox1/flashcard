8baab34c4866175e70ad11968d41724b1ad1e8c4
SrFox <136258502+GabrielFox1@users.noreply.github.com>
2024-12-10 18:43:05 -0300

    perguntas.js

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCarta(
+    'Desenvolvimento',
+    'O que é Python?',
+    'Python é uma linguagem de programação'
+)
+
+criaCarta(
+    'Geografia',
+    'Qual é a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCarta(
+    'Desenvolvimento',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa uma tarefa específica'
+)
+
+criaCarta(
+    'Idioma Inglês',
+    'Como se diz "oi" em inglês?',
+    'Oi em inglês é "HI" (pronunciado como RAI)'
+)
